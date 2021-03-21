002939407b05f5107f864e28d066034a65cd8ed1
nalijagic1 <nalijagic1@etf.unsa.ba>
2021-03-21 18:57:31 +0100

    Kopiran tutorijal 1

--- src/ba/unsa/etf/rpr/GradController.java
+++ src/ba/unsa/etf/rpr/GradController.java
@@ -1,20 +1,40 @@
 package ba.unsa.etf.rpr;
 
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+
 
 public class GradController {
     public TextField fieldNaziv;
     public TextField fieldBrojStanovnika;
     public ChoiceBox<Drzava> choiceDrzava;
     public ObservableList<Drzava> listDrzave;
+    public TextField fldPostanskiBroj;
+    public ImageView Img;
     private Grad grad;
 
     public GradController(Grad grad, ArrayList<Drzava> drzave) {
@@ -28,6 +48,10 @@ public class GradController {
         if (grad != null) {
             fieldNaziv.setText(grad.getNaziv());
             fieldBrojStanovnika.setText(Integer.toString(grad.getBrojStanovnika()));
+            fldPostanskiBroj.setText(Integer.toString(grad.getPostanskiBroj()));
+            Image im = new Image(grad.getSlika());
+            Img.setImage(im);
+
             // choiceDrzava.getSelectionModel().select(grad.getDrzava());
             // ovo ne radi jer grad.getDrzava() nije identički jednak objekat kao član listDrzave
             for (Drzava drzava : listDrzave)
@@ -47,42 +71,79 @@ public class GradController {
         Stage stage = (Stage) fieldNaziv.getScene().getWindow();
         stage.close();
     }
+    public void promijeniSliku(ActionEvent actionEvent){
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setContentText("Unesite putanju do slike");
+        dialog.showAndWait();
+        String putanja = dialog.getEditor().getText();
+        Image i = new Image(putanja);
+        Img.setImage(i);
+    }
 
     public void clickOk(ActionEvent actionEvent) {
-        boolean sveOk = true;
 
-        if (fieldNaziv.getText().trim().isEmpty()) {
-            fieldNaziv.getStyleClass().removeAll("poljeIspravno");
-            fieldNaziv.getStyleClass().add("poljeNijeIspravno");
-            sveOk = false;
-        } else {
-            fieldNaziv.getStyleClass().removeAll("poljeNijeIspravno");
-            fieldNaziv.getStyleClass().add("poljeIspravno");
-        }
+        Thread t = new Thread(() -> {
+            AtomicBoolean sveOk = new AtomicBoolean(true);
+            if (fieldNaziv.getText().trim().isEmpty()) {
+                fieldNaziv.getStyleClass().removeAll("poljeIspravno");
+                fieldNaziv.getStyleClass().add("poljeNijeIspravno");
+                sveOk.set(false);
+            } else {
+                fieldNaziv.getStyleClass().removeAll("poljeNijeIspravno");
+                fieldNaziv.getStyleClass().add("poljeIspravno");
+            }
+            int brojStanovnika = 0;
+            try {
+                brojStanovnika = Integer.parseInt(fieldBrojStanovnika.getText());
+            } catch (NumberFormatException e) {
+                // ...
+            }
+            if (brojStanovnika <= 0) {
+                fieldBrojStanovnika.getStyleClass().removeAll("poljeIspravno");
+                fieldBrojStanovnika.getStyleClass().add("poljeNijeIspravno");
+                sveOk.set(false);
+            } else {
+                fieldBrojStanovnika.getStyleClass().removeAll("poljeNijeIspravno");
+                fieldBrojStanovnika.getStyleClass().add("poljeIspravno");
+            }
+            try {
+                URL url = new URL("http://c9.etf.unsa.ba/proba/postanskiBroj.php?postanskiBroj=" + fldPostanskiBroj.getText());
+                BufferedReader ulaz = new BufferedReader(new InputStreamReader(url.openStream(), StandardCharsets.UTF_8));
+                String json = "", line = null;
+                while ((line = ulaz.readLine()) != null)
+                    json = json+line;
+                System.out.println(json);
+                if (json.equals("NOT OK")) {
+                        fldPostanskiBroj.getStyleClass().removeAll("poljeIspravno");
+                        fldPostanskiBroj.getStyleClass().add("poljeNijeIspravno");
+                }else{
+                    Platform.runLater(() -> {
+                    fldPostanskiBroj.getStyleClass().removeAll("poljeNijeIspravno");
+                    fldPostanskiBroj.getStyleClass().add("poljeIspravno");
+                    if (!sveOk.get()) return;
 
+                        if (grad == null) grad = new Grad();
+                        grad.setNaziv(fieldNaziv.getText());
+                        grad.setBrojStanovnika(Integer.parseInt(fieldBrojStanovnika.getText()));
+                        grad.setDrzava(choiceDrzava.getValue());
+                        grad.setPostanskiBroj(Integer.parseInt(fldPostanskiBroj.getText()));
+                        Stage stage = (Stage) fieldNaziv.getScene().getWindow();
+                        stage.close();});
+                }
 
-        int brojStanovnika = 0;
-        try {
-            brojStanovnika = Integer.parseInt(fieldBrojStanovnika.getText());
-        } catch (NumberFormatException e) {
-            // ...
-        }
-        if (brojStanovnika <= 0) {
-            fieldBrojStanovnika.getStyleClass().removeAll("poljeIspravno");
-            fieldBrojStanovnika.getStyleClass().add("poljeNijeIspravno");
-            sveOk = false;
-        } else {
-            fieldBrojStanovnika.getStyleClass().removeAll("poljeNijeIspravno");
-            fieldBrojStanovnika.getStyleClass().add("poljeIspravno");
-        }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }});
+        t.start();
+    }
 
-        if (!sveOk) return;
-
-        if (grad == null) grad = new Grad();
-        grad.setNaziv(fieldNaziv.getText());
-        grad.setBrojStanovnika(Integer.parseInt(fieldBrojStanovnika.getText()));
-        grad.setDrzava(choiceDrzava.getValue());
-        Stage stage = (Stage) fieldNaziv.getScene().getWindow();
-        stage.close();
+    public void pretraga(ActionEvent actionEvent) throws IOException {
+        Stage stage=new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/pretraga.fxml"));
+        stage.setTitle("Pretraga datoteke");
+        stage.setScene(new Scene(root, Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE));
+        stage.show();
     }
 }
---
+++ src/ba/unsa/etf/rpr/PretragaController.java
@@ -0,0 +1,77 @@
+package ba.unsa.etf.rpr;
+
+import com.sun.javafx.charts.Legend;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import javax.swing.filechooser.FileSystemView;
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class PretragaController implements Initializable
+{
+    public TextField fldPretraga;
+    private static ObservableList<String> direktoriji = FXCollections.observableArrayList();
+    private static  String naziv ="";
+    public ListView listPutanje;
+    int velicina = 0;
+
+    public static void dodajUListu(){
+
+    }
+    public static void searchFile(File f){
+        Thread t = new Thread(() -> {
+            try
+            {
+                if(f.isDirectory())
+                {
+                    File [] fi = f.listFiles();
+                    for(int i=0;i<fi.length;i++)
+                    {
+                        searchFile(fi[i]);
+                    }
+                }
+                else
+                    {
+                        if(f.getName().toLowerCase().contains(naziv.toLowerCase()))
+                    {
+                        Platform.runLater(() -> {
+                            //System.out.print("file found " + f.getAbsolutePath());
+                            direktoriji.add(f.getAbsolutePath());
+                        });
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+            }
+        });
+        t.start();
+    }
+    public void pretrazi(ActionEvent actionEvent) {
+        naziv = fldPretraga.getText();
+        searchFile( FileSystemView.getFileSystemView().getHomeDirectory());
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        direktoriji.addListener(new ListChangeListener<String>() {
+            @Override
+            public void onChanged(javafx.collections.ListChangeListener.Change<? extends String> c) {
+                listPutanje.setItems(direktoriji);
+
+            }
+
+        });
+    }
+}
