751900453764152530f818222d23d54e85cd85a0
nalijagic1 <nalijagic1@etf.unsa.ba>
2021-03-22 11:11:42 +0100

    Zaboravljena izmijenuta baza u prošlom commitu

--- baza.db.sql
+++ baza.db.sql
@@ -4,14 +4,16 @@ CREATE TABLE IF NOT EXISTS `grad` (
 	`naziv`	TEXT,
 	`broj_stanovnika`	INTEGER,
 	`drzava`	INTEGER,
+    `postanski_broj`	INTEGER,
+    `slika`	TEXT,
 	FOREIGN KEY(`drzava`) REFERENCES `drzava`,
 	PRIMARY KEY(`id`)
 );
-INSERT INTO `grad` VALUES (1,'Pariz',2206488,1);
-INSERT INTO `grad` VALUES (2,'London',8825000,2);
-INSERT INTO `grad` VALUES (3,'Beč',1899055,3);
-INSERT INTO `grad` VALUES (4,'Manchester',545500,2);
-INSERT INTO `grad` VALUES (5,'Graz',280200,3);
+INSERT INTO `grad` VALUES (1,'Pariz',2206488,1,71000,'/img/unknown.jpg');
+INSERT INTO `grad` VALUES (2,'London',8825000,2,71000,'/img/unknown.jpg');
+INSERT INTO `grad` VALUES (3,'Beč',1899055,3,71000,'/img/unknown.jpg');
+INSERT INTO `grad` VALUES (4,'Manchester',545500,2,71000,'/img/unknown.jpg');
+INSERT INTO `grad` VALUES (5,'Graz',280200,3,71000,'/img/unknown.jpg');
 CREATE TABLE IF NOT EXISTS `drzava` (
 	`id`	INTEGER,
 	`naziv`	TEXT,
